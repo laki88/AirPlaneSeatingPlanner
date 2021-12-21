e9650023bac918eddb6f9a648c352cbdbc048b5e
lakshmanu <lakshmanu@lseg.com>
2021-12-21 16:32:22 +0530

    Add presentation code

--- src/main/java/org/laki/beans/SeatType.java
+++ src/main/java/org/laki/beans/SeatType.java
@@ -1,7 +1,17 @@
 package org.laki.beans;
 
 public enum SeatType {
-    WINDOW_SEAT,
-    MIDDLE_SEAT,
-    AISLE_SEAT
+    WINDOW_SEAT("W"),
+    MIDDLE_SEAT("M"),
+    AISLE_SEAT("A");
+
+    public final String label;
+
+    SeatType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
--- src/main/java/org/laki/frontend/ConsolePresenter.java
+++ src/main/java/org/laki/frontend/ConsolePresenter.java
@@ -1,21 +1,45 @@
 package org.laki.frontend;
 
+import de.vandermeer.asciitable.AsciiTable;
+import org.laki.beans.Passenger;
 import org.laki.beans.Seat;
 import org.laki.beans.SeatGroup;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class ConsolePresenter implements Presenter {
 
     @Override
     public void printSeatingArrangement(List<SeatGroup> seatGroups) {
-        seatGroups.forEach(seatGroup -> {
-            //todo add table printing here
-        });
+        printSeating(seatGroups);
+    }
+
+    private void printSeating(List<SeatGroup> seatGroups) {
+        AsciiTable at = new AsciiTable();
+        at.addRule();
+        List<String> insideBoxes = new ArrayList<>();
+        for (SeatGroup seatGroup : seatGroups) {
+            Seat[][] seats = seatGroup.getSeats();
+            AsciiTable seatGroupAsciiTable = new AsciiTable();
+            for (int i = 0; i < seats.length; i++) {
+                List<String> rowInSeatGroup = new ArrayList<>();
+                for (int j = 0; j < seats[i].length;j++) {
+                    String seatLabel = seats[i][j].getSeatType().getLabel();
+                    Passenger passenger = seats[i][j].getPassenger();
+                    String seqNo = "";
+                    if (passenger != null) {
+                        seqNo += passenger.getSeqNo();
+                    }
+                    rowInSeatGroup.add(seatLabel + " : " + seqNo);
+                }
+                seatGroupAsciiTable.addRow(rowInSeatGroup);
+            }
+            insideBoxes.add(seatGroupAsciiTable.render());
+        }
+        at.addRow(insideBoxes);
+        at.addRule();
+        System.out.println(at.render());
     }
 
 }
